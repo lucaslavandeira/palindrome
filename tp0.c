107c1ee12c14e6ba07900548138393ee72458090
BlancoSebastianEzequiel <sebastian.e.blanco@gmail.com>
2017-09-07 00:54:37 -0300

    Add files via upload

--- tp0.c
+++ tp0.c
@@ -2,9 +2,13 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
+#include <stdlib.h>
 //------------------------------------------------------------------------------
 // DEFINITIONS
 //------------------------------------------------------------------------------
+#define ERROR -1
+#define SUCCESS 0
+#define INITIAL 100
 #define VERSION "0.1"
 const char help_str[] = "Usage:\n"
         "  tp0 -h\n"
@@ -20,9 +24,13 @@ const char help_str[] = "Usage:\n"
 #define SPACE_SIZE 65
 #define SPACE_INDEX 123
 #define EMPTY -1
-const char* ENTER = "\n";
+const char ENTER = '\n';
 char space[SPACE_SIZE];
 int spaceIndex[SPACE_INDEX];
+const char version[] = "--version";
+const char help[] = "--help";
+const char input[] = "--input";
+const char output[] = "--output";
 //------------------------------------------------------------------------------
 // CHARGE SPACE
 //------------------------------------------------------------------------------
@@ -71,48 +79,86 @@ bool belongsToSpace(int aChar) {
     return spaceIndex[aChar] != EMPTY;
 }
 //------------------------------------------------------------------------------
-// IS CAPICUA
+// WRITE OUT FILE
 //------------------------------------------------------------------------------
-bool isCapicua(char* word, size_t size) {
-    if (size == 0) return false;
-    if (size == 1) return true;
-    size_t rightPos = 0, leftPos = size-1;
-    while (rightPos < leftPos) {
-        int a = tolower(word[rightPos]);
-        int b = tolower(word[leftPos]);
-        if (a != b) return false;
-        rightPos++;
-        leftPos--;
+int writeOutFile(FILE* archIn, long begin, long end, FILE* archOut) {
+    int c;
+    if (fseek(archIn, begin, SEEK_SET) < 0) return ERROR;
+    for (long i = 0; i < (end - begin); i++) {
+        if (fread((char*)(&c), sizeof(char), 1, archIn) <= 0) return ERROR;
+        if (fwrite((char*)(&c), sizeof(char), 1, archOut) <= 0) return ERROR;
     }
-    return true;
+    if (fwrite((char*)(&ENTER), sizeof(char), 1, archOut) <= 0) return ERROR;
+    return SUCCESS;
+}
+//------------------------------------------------------------------------------
+// READ LETTER
+//------------------------------------------------------------------------------
+int readLetter(FILE* arch, long pos, char* letter) {
+    if (fseek(arch, pos, SEEK_SET) == ERROR) return ERROR;
+    if (fread(letter, sizeof(char), 1, arch) <= 0) return ERROR;
+    return SUCCESS;
+}
+//------------------------------------------------------------------------------
+// CAPICUA
+//------------------------------------------------------------------------------
+int capicua(FILE *archIn, long begin, long end, FILE *archOut) {
+    char letterA, letterB;
+    bool isCapicua = true;
+    long size = (end - begin);
+    long leftPos = begin, rightPos = end-1;
+    if (size == 0) return SUCCESS;
+    while (leftPos <= rightPos) {
+        if (readLetter(archIn, leftPos, &letterA) == ERROR) return ERROR;
+        if (readLetter(archIn, rightPos, &letterB) == ERROR) return ERROR;
+        int a = tolower(letterA);
+        int b = tolower(letterB);
+        if (a != b) {
+            isCapicua = false;
+            break;
+        }
+        rightPos--;
+        leftPos++;
+    }
+    if (!isCapicua) return SUCCESS;
+    if (writeOutFile(archIn, begin, end, archOut) == ERROR) return ERROR;
+    return SUCCESS;
 }
 //------------------------------------------------------------------------------
 // READ FILE
 //------------------------------------------------------------------------------
-void readFile(FILE* archIn, FILE* archOut) {
-    fseek(archIn, 0, SEEK_SET);
-    fseek(archOut, 0, SEEK_SET);
+int readFile(FILE* archIn, FILE* archOut) {
+    if (fseek(archIn, 0, SEEK_END) == ERROR) return ERROR;
+    if (ftell(archIn) == 0) return SUCCESS;
+    if (fseek(archIn, 0, SEEK_SET) == ERROR) return ERROR;
+    if (fseek(archOut, 0, SEEK_SET) == ERROR) return ERROR;
+    long begin = ftell(archIn);
+    long end = begin;
+    long last;
     int c = getc(archIn);
-    size_t size = 0;
-    char word[100];
+    bool first = false;
     while (c != EOF) {
         if (belongsToSpace(c)) {
-            word[size] = (char)c;
-            size++;
-        } else {
-            if (isCapicua(word, size)) {
-                fwrite(word, sizeof(char)*size, 1, archOut);
-                fwrite(ENTER, sizeof(char)*1, 1, archOut);
+            if (!first) {
+                first = true;
+                begin = ftell(archIn) - 1;
             }
-            size = 0;
+            end = ftell(archIn);
+        } else {
+            first = false;
+            last = ftell(archIn);
+            if (capicua(archIn, begin, end, archOut) == ERROR) return ERROR;
+            begin = end;
+            if (fseek(archIn, last, SEEK_SET) == ERROR) return ERROR;
         }
         c = getc(archIn);
     }
+    return SUCCESS;
 }
 //------------------------------------------------------------------------------
-// Parsea los argumentos
+// ARG PARSE
 //------------------------------------------------------------------------------
-void arg_parse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
+int arg_parse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
     int arg = 1;
     const char flags[] = {'i', 'o', 'V', 'h'};
     char flag = 0;
@@ -128,28 +174,31 @@ void arg_parse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
             if (flag == 'h') {
                 printf("%s\n", help_str);
                 *clean_exit = 1;
-                return;
+                return SUCCESS;
             }
             if (flag == 'V') {
                 printf("tp0: version %s\n", VERSION);
                 *clean_exit = 1;
-                return;
+                return SUCCESS;
             }
             if (!flag) {
                 printf("Invalid argument: %s", argv[arg]);
                 descriptors[0] = NULL;
-                return;
+                return SUCCESS;
             }
         } else {
             if (flag == 'i') {
                 descriptors[0] = fopen(argv[arg], "r");
+                if (descriptors[0] == NULL) return ERROR;
             } else if (flag == 'o') {
                 descriptors[1] = fopen(argv[arg], "w");
+                if (descriptors[1] == NULL) return ERROR;
             }
             flag = 0;
         }
         arg++;
     }
+    return SUCCESS;
 }
 //------------------------------------------------------------------------------
 // MAIN
@@ -157,14 +206,16 @@ void arg_parse(int argc, char** argv, FILE** descriptors, int* clean_exit) {
 int main(int argc, char** argv) {
     FILE* fdescriptors[2] = {stdin, stdout};
     int clean_exit = 0;
-    arg_parse(argc, argv, fdescriptors, &clean_exit);
-    if (clean_exit) return 0; // finalizacion limpia, cuando se usa -h o -V
-    if (!fdescriptors[0] || !fdescriptors[1]) return 1;
-
+    if (arg_parse(argc, argv, fdescriptors, &clean_exit) == ERROR) return 1;
+    if (clean_exit) return 0;  // finalizacion limpia, cuando se usa -h o -V
     chargeSpace();
-    readFile(fdescriptors[0], fdescriptors[1]);
-    if (fdescriptors[0] != stdin) fclose(fdescriptors[0]);
-    if (fdescriptors[1] != stdout) fclose(fdescriptors[1]);
+    if (readFile(fdescriptors[0], fdescriptors[1]) == ERROR) return 1;
+    if (fdescriptors[0] != stdin) {
+        if (fclose(fdescriptors[0]) == EOF) return 1;
+    }
+    if (fdescriptors[1] != stdout) {
+        if (fclose(fdescriptors[1]) == EOF) return 1;
+    }
     return 0;
 }
 //------------------------------------------------------------------------------
